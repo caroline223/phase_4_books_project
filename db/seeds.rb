656987c3fce7ac0629e17144aad628eb6233542a
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-10-16 14:16:26 -0400

    Added the faker and CORS gems to the app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Author.destroy_all
+Book.destroy_all
+BookReview.destroy_all
+
+30.times do
+    Author.create(name: Faker::Name.name, 
+                 birth_city: Faker::Address.city,
+                 birth_state: Faker::Address.state,
+                 birth_date: Faker::Date.backward(days: 30000),
+                 college: Faker::Educator.university,
+                 degree: Faker::Educator.degree,
+                )
+end
+
+200.times do 
+    id = Author.all.sample.id
+    Book.create(title: Faker::Book.title, 
+                genre: Faker::Book.genre, 
+                publishing_date: Faker::Date.backward(days: 15000), 
+                author_id: id
+                rating: Faker::Number.within(range: 1..10))
+     
+    5.times do
+        BookReview.create(
+            review: Faker::Hipster.sentences(number: 4)
+        )
+    end
+
+end
