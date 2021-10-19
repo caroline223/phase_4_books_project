2b1a0e37521c38ec40d3995228e065ef2c39381c
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-10-19 12:59:38 -0400

    Wrote a custom action and route for the authors_controller to link the books written by each author.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -5,10 +5,31 @@ class AuthorsController < ApplicationController
         render json: authors 
     end
 
+    
     def show 
-        author = Author.find(params[:id])
+        author = find_author 
         render json: author
     end
 
+    def author_books
+        author = find_author
+        author_books = find_author.books.map do |book|
+            {
+                title: book.title,
+                genre: book.genre,
+                publishing_date: book.publishing_date,
+                rating: book.rating 
+            }
+            
+        end
+        render json: author_books
+    end
+
+
+    private
+
+    def find_author
+        Author.find(params[:id])
+    end
 
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,17 @@
 class BooksController < ApplicationController
 
     def index
-        books = Book.all.map do |book|
-            { 
-                id: book.id,
-                title: book.title,
-                genre: book.genre,
-                author_name: book.author.name,
-                publishing_date: book.publishing_date,
-                rating: book.rating,
-                image_url: book.image_url,
-                author_id: book.author_id, 
-            }
-        end 
+        books = Book.all
         render json: books 
     end
 
+    
     def show 
         book = find_book
-        render json: book 
+        render json: book
     end
 
-
+   
     private
 
     def find_book
