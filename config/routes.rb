349f1efd89ff381fed80b66157712ea60d551b6c
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-10-19 13:02:36 -0400

    Added a custom route to the authors controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
   resources :book_reviews
   resources :authors
   resources :books
+
+  get "authors/books/:id", to: "authors#author_books"
+  
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  
+  
+  
+
 end
