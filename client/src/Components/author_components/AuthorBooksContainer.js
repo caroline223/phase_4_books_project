0f43465d61e87f032e11c203ab1a058d79f7ba1d
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-10-19 13:04:47 -0400

    Added the AuthorBooks Container and Info components to display the books that any particular author has wrote

---
+++ client/src/Components/author_components/AuthorBooksContainer.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+import { Card, Button } from 'semantic-ui-react'
+import AuthorBooksInfo from './AuthorBooksInfo'
+import NavBar from '../NavBar'
+
+
+
+function AuthorBooksContainer({ match }) {
+
+    const [data, setData] = useState([])
+   
+    useEffect(() => {
+        fetch(`http://localhost:3000/authors/books/${match.params.id}`)
+        .then((response) => response.json())
+        .then((data) => setData(data))
+    }, [match.params.id])
+
+
+    const booksDisplay = () => data.map((d) => <AuthorBooksInfo key={d.id} data={d} />)
+
+    
+    return(
+        <div>
+            <NavBar />
+                <h1>Author's Writing Collection</h1> 
+                <br />
+                <Card.Group >
+                    {data.length > 0 && booksDisplay()}
+                </Card.Group>
+                <div>
+                    <Button a href="http://localhost:4000/authors">Back</Button>
+                </div>
+        </div>
+    )
+    
+   
+}
+
+export default AuthorBooksContainer;
---
+++ client/src/Components/author_components/AuthorBooksInfo.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Card, Image } from 'semantic-ui-react'
+import bookPhoto from '../home_components/BookPhoto'
+
+function AuthorBooksInfo(props) {
+
+
+    const layout = {
+        card : {
+            opacity: "0.8",
+            borderRadius: "40px"
+        }
+    }
+
+    const { title, genre, publishing_date, rating } = props.data
+
+    return(
+        <div>
+         <Card style={layout.card} color='olive'>
+            <Image src={bookPhoto[Math.floor(Math.random()*bookPhoto.length)]} width="300" height="200"/> 
+            <Card.Content>
+              <header>
+              {title} 
+              </header>
+              <Card.Description>
+                  Genre: {genre}
+                 <div>
+                    Date Published: {publishing_date}
+                 </div>
+                  <div>
+                    Rating: {rating}/10
+                  </div>
+                </Card.Description>
+                </Card.Content>
+        </Card>
+      </div>
+    )
+
+
+}
+export default AuthorBooksInfo;
