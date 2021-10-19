6d0d061e7fcfd46138eb495b0be211f74c7299ab
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-10-19 13:08:29 -0400

    Added routes to the App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import BookContainer from './Components/home_components/BookContainer';
 import AuthorContainer from './Components/author_components/AuthorContainer';
 import Author from './Components/home_components/Author'
-import AuthorBooks from './Components/author_components/AuthorBooks'
+import BookReviewsContainer from './Components/home_components/BookReviewsContainer';
+import LoginForm from './Components/user_components/LoginForm';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import AuthorBooksContainer from './Components/author_components/AuthorBooksContainer';
+
 
 function App() {
   return (
@@ -12,7 +15,9 @@ function App() {
        <Route exact path="/books" component={BookContainer} />
        <Route exact path="/authors" component={AuthorContainer} />
        <Route exact path="/authors/:id" component={Author} />
-       <Route exact path="/books/:id" component={AuthorBooks} />
+       <Route exact path="/books/:id" component={BookReviewsContainer} />
+       <Route exact path="/authors/books/:id" component={AuthorBooksContainer} />
+       <Route exact path="/login" component={LoginForm} />
      </Switch>
    </Router>
   );
