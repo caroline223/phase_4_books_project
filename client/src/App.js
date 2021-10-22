1c04957cf88e3b79e40d0305072da57873bfdcd8
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-10-21 22:26:20 -0400

    Added the HomePage component to the Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import BookContainer from './Components/home_components/BookContainer';
+import BookReviewsContainer from './Components/home_components/BookReviewsContainer';
 import AuthorContainer from './Components/author_components/AuthorContainer';
 import Author from './Components/home_components/Author'
-import BookReviewsContainer from './Components/home_components/BookReviewsContainer';
-import LoginForm from './Components/user_components/LoginForm';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import AuthorBooksContainer from './Components/author_components/AuthorBooksContainer';
+import LoginForm from './Components/user_components/LoginForm';
+import SignUpForm from './Components/user_components/SignUpForm';
+import HomePage from './Components/HomePage';
 
 
 function App() {
   return (
    <Router>
      <Switch>
+       <Route exact path ="/home" component={HomePage} />
        <Route exact path="/books" component={BookContainer} />
        <Route exact path="/authors" component={AuthorContainer} />
        <Route exact path="/authors/:id" component={Author} />
        <Route exact path="/books/:id" component={BookReviewsContainer} />
        <Route exact path="/authors/books/:id" component={AuthorBooksContainer} />
        <Route exact path="/login" component={LoginForm} />
+       <Route exact path ="/sign_up" component={SignUpForm} />
      </Switch>
    </Router>
   );
