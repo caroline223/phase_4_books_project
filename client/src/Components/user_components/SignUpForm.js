18b9102093e16477c35cf3673da0b3903aa1319b
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-10-21 22:41:31 -0400

    Added the NavBar componeny to the sign-up form

--- client/src/Components/user_components/SignUpForm.js
+++ client/src/Components/user_components/SignUpForm.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Button, Form, Message } from 'semantic-ui-react'
+import NavBar from '/Users/carolineforrester/Flatiron/code/phase_4/project/project-template-react-rails-api/client/src/Components/NavBar.js'
 
 const SignUpForm = () => (
     <div>
+        <NavBar />
+        <br />
         <h1>Create An Account</h1>
     <Form success>
         <Form.Input label='First Name' placeholder='first name' />
@@ -17,7 +20,6 @@ const SignUpForm = () => (
             content="You're all signed up! Please return to the login page to access your new account."
         />   
     <Button>Submit</Button>
-    <Button a href="http://localhost:4000/home">Home</Button>
   </Form>
   </div>
 
