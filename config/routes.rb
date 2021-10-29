161fdc747dd51c645d12851e2715c453dc5610de
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-10-29 02:28:54 -0400

    Edited the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
   
-  resources :user_personal_books
-  resources :personal_books
-  resources :user_personal_recommendations
-  resources :personal_recommendations
+  resources :authors, only: [:index, :show]
+  resources :user_books, only: [:index, :update, :destroy]
+  
+  resources :books do 
+    resources :user_books, only: [:create]
+  end
+
   resources :users
   resources :book_reviews
-  resources :authors
-  resources :books
+  
+  
 
   get "authors/books/:id", to: "authors#author_books"
   
+ 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
