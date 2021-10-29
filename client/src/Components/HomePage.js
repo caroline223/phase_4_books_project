3a092ecd642963288b7a11cb5b8a0736d74e7478
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-10-29 12:21:32 -0400

    Added the features for the home page:

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -1,11 +1,44 @@
 import React from 'react'
+import { Card, Image } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import bookPhoto from './home_components/BookPhoto'
 
 
 const HomePage = () => {
 
-    return (
+    const layout = {
+        card : {
+            opacity: "0.8",
+            borderRadius: "40px"
+        }
+    }
+
+    return(
         <div>
-             
+            <div>
+                <br />
+                <div >
+                    Simply Books
+                </div>
+                <br /> <br /> <br /> <br /> <br />
+                <div>
+                    <p>Book Enthusiasts! Welcome your very own, highly personalized library app!</p>
+                    <p>Click on the link below to either view our recommendation of some entertaining books,</p>
+                    <p>as well as upload your own personal library!</p>
+                </div>
+                <br /><br />
+                <div>
+                    <Card style={layout.card} color='olive'>
+                        <Image src={bookPhoto[Math.floor(Math.random()*bookPhoto.length)]} width="300" height="200" />
+                            <Card.Content>
+                                <Link to={"/login"}>Login</Link>
+                        </Card.Content>
+                    </Card>  
+                </div>
+                            
+                     
+              
+            </div>
         </div>
     )
 
