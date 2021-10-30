eb75527df285e34f15ca6edbf6b6f4d2f8ccc7d0
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-10-30 01:03:55 -0400

    Updated the update action in the userbooks controller to render json

--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -8,9 +8,15 @@ class UserBooksController < ApplicationController
         current_user.user_books.create(book_id: params[:book_id])
     end
 
+    def show
+        user_book = find_user_book
+        render json: user_book 
+    end
+
     def update
         user_book = find_user_book
         user_book.update(user_book_params)
+        render json: user_book
     end
 
     def destroy
