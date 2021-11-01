b1743265fe262b602d668455fdd7cfdc46033988
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-11-01 12:04:34 -0400

    Added the validations for the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
     
     has_many :user_books
     has_many :books, through: :user_books
+
+    validates :email, uniqueness: true, allow_blank: false
+    validates :username, presence: true, uniqueness: true 
 end
