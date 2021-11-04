9e49bbfd6c1a2a7aa473cffa6a44cd9ccc2353b1
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-11-04 18:39:06 -0400

    Minor updates

--- db/migrate/20211025012036_drop_personal_libraries.rb
+++
@@ -1,5 +0,0 @@
-class DropPersonalLibraries < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :personal_libraries 
-  end
-end
--- db/migrate/20211025012137_drop_user_personal_libraries.rb
+++
@@ -1,5 +0,0 @@
-class DropUserPersonalLibraries < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :user_personal_libraries
-  end
-end
