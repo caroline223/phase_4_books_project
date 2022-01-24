e8c128014969058ae86a3fd3c38404bfb21ea195
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-01-23 21:54:16 -0500

    Added the destroy action to the users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,15 @@ class UsersController < ApplicationController
         end
    end
 
+   def destroy
+        if @current_user
+            @current_user.destroy
+            head :no_content
+        else
+            render json: {}, status: :unauthorized 
+        end
+   end
+
     private 
 
     def user_params 
