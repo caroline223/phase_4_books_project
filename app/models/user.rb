084379183f30d8dd7b99ca6fdd9cb7ab23e7598b
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-01-29 22:48:36 -0500

    Updated the validations for the user.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     has_many :user_books
     has_many :books, through: :user_books
 
-    validates :email, uniqueness: true, allow_blank: false
-    validates :username, presence: true, uniqueness: true 
-    validates :password, presence: true
+    validates :email, presence: true, uniqueness: true, allow_blank: false
+    validates :username, presence: true, uniqueness: true, length: { in: 6..30 }
+    validates :password, presence: true, length: { in: 6..20 }
 end
